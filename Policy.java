eb8cd4bb52ed463f9157e7a08b69ce0b6e790f7e
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-04-08 20:09:19 -0400

    Update Policy.java
    Added toString method

--- Policy.java
+++ Policy.java
@@ -7,12 +7,6 @@ public class Policy
    //Create all necessary fields, declared as private to encapsulate data
    private String policyNumber;
    private String providerName;
-   //private String holderFirstName;
-   //private String holderLastName;
-   //private int holderAge;
-   //private String smokingStatus;
-   //private double holderHeight;
-   //private double holderWeight;
    
    /**
       No-arg constructor
@@ -24,12 +18,6 @@ public class Policy
    {
       policyNumber = "";
       providerName = "";
-      //holderFirstName = "";
-      //holderLastName = "";
-      //holderAge = 0;
-      //smokingStatus = "";
-      //holderHeight = 0;
-      //holderWeight = 0;
    }
    
    /**
@@ -39,27 +27,14 @@ public class Policy
       
       @param polNum     The holders policy number
       @param provName   The name of the insurance provider
-      @param first      The policy holders first name
-      @param last       The policy holders last name
-      @param age        The policy holders age
-      @param smokeStat  The policy holders smoking status
-      @param height     The policy holders height in inches
-      @param weight     The policy holders weight in pounds
    */
-   public Policy(String polNum, String provName)/*, String first, String last,
-                 int age, String smokeStat, double height, double weight)*/
+   public Policy(String polNum, String provName)
    {
       policyNumber = polNum;
       providerName = provName;
-      //holderFirstName = first;
-      //holderLastName = last;
-      //holderAge = age;
-      //smokingStatus = smokeStat;
-      //holderHeight = height;
-      //holderWeight = weight;
    }
    
-   //Start accessor methods
+   //Start mutator methods
    
    /**
       The setPolicyNumber method allows the policy number to be set with a given argument
@@ -82,76 +57,9 @@ public class Policy
    {
       providerName = pName;
    }
-   
-   /**
-      The setHolderFirstName method allows the holder first name to be set with a given argument
-      
-      @param fName     The holder first name to be set
-   */
-   
-   //public void setHolderFirstName(String fName)
-   //{
-   //   holderFirstName = fName;
-   //}
-   
-   /**
-      The setHolderLastName method allows the holder last name to be set with a given argument
-      
-      @param lName     The holder last name to be set
-   */
-   
-   //public void setHolderLastName(String lName)
-   //{
-   //   holderLastName = lName;
-   //}
-   
-   /**
-      The setHolderAge method allows the holder age to be set with a given argument
-      
-      @param a     The holder age to be set
-   */
-   
-   //public void setHolderAge(int a)
-   //{
-   //   holderAge = a;
-   //}
-   
-   /**
-      The setSmokingStatus method allows the holder smoking status to be set with a given argument
-      
-      @param smoke     The holder smoking status to be set
-   */
-   
-   //public void setSmokingStatus(String smoke)
-   //{
-   //  smokingStatus = smoke;
-   //}
-
-   /**
-      The setHolderHeight method allows the holder height to be set with a given argument
-      
-      @param h     The holder height to be set
-   */
-   
-   //public void setHolderHeight(double h)
-   //{
-   //   holderHeight = h;
-   //}
-   
-   /**
-      The setHolderWeight method allows the holder weight to be set with a given argument
-      
-      @param w     The holder weight to be set
-   */
-   
-   //public void setHolderWeight(double w)
-   //{
-   //   holderWeight = w;
-   //}
-
-   //End accessor methods
-   
-   //Start mutator methods
+   //End mutator methods
+     
+   //Start accessor methods
    
    /**
       The getPolicyNumber method gets the value stored in the policyNumber field
@@ -175,100 +83,13 @@ public class Policy
       return providerName;
    }
    
-   /**
-      The getHolderFirstName method gets the value stored in the holderFirstName field
-      
-      @return     The value stored in the holderFirstName field
-   */
-   
-   //public String getHolderFirstName()
-   //{
-   //   return holderFirstName;
-   //}
-   
-   /**
-      The getHolderLastName method gets the value stored in the holderLastName field
-      
-      @return     The value stored in the holderLastName field
-   */
-   
-   //public String getHolderLastName()
-   //{
-   //   return holderLastName;
-   //}
-   
-   /**
-      The getHolderAge method gets the value stored in the holderAge field
-      
-      @return     The value stored in the holderAge field
-   */
-   
-   //public int getHolderAge()
-   //{
-   //  return holderAge;
-   //}
-   
-   /**
-      The getSmokingStatus method gets the value stored in the smokingStatus field
-      
-      @return     The value stored in the smokingStatus field
-   */
-   
-   //public String getSmokingStatus()
-   //{
-   //   return smokingStatus;
-   //}
-   
-   /**
-      The getHolderHeight method gets the value stored in the holderHeight field
-      
-      @return     The value stored in the holderHeight field
-   */
-   
-   //public double getHolderHeight()
-   //{
-   //   return holderHeight;
-   //}
-   
-   /**
-      The getHolderWeight method gets the value stored in the holderWeight field
-      
-      @return     The value stored in the holderWeight field
-   */
-   
-   //public double getHolderWeight()
-   //{
-   //   return holderWeight;
-   //}
-   
-   //End mutator methods
+   //End accessor methods
    
    //Start calculation methods
-   
-   /**
-      The getHolderBmi method calculates the holder BMI based on the holderHeight and holderWeight fields
       
-      @param height     The policy holder height
-      @param weight     The policy holder weight
-      
-      @return           The policy holders calculated BMI
-   */
-   
-   //public double getHolderBmi()
-   //{
-   //   final int CONVERSION_FACTOR = 703;
-   //   
-   //   return (holderWeight * CONVERSION_FACTOR) / (holderHeight * holderHeight);
-   //}
-   
    /**
       The getPolicyPrice method calculates the policy price based on the holderAge, 
-      smokingStatus, holderHeight, sholderWeight fields and the calculated holder BMI
-      
-      @param age     The policy holder age     
-      @param smoke   The policy holder smoking status
-      @param height  The policy holder height
-      @param weight  The policy holder weight     
+      smokingStatus, holderHeight, sholderWeight fields and the calculated holder BMI     
       
       @return        The calculated policy price
    */
@@ -312,24 +133,20 @@ public class Policy
    
    //End calculation methods
    
-   //Start display method
+   //Start toString method
    
    /**
-      The display Information method displays all information related to a users policy.
+      The toString method returns the string representation of an object.
    */
-   public void displayInformation()
+   public String toString()
    {
-      System.out.println("\nPolicy Number: " + getPolicyNumber());
-      System.out.println("\nProvider Name: " + getProviderName());
-      System.out.println("\nPolicyholder's First Name: " + getHolderFirstName());
-      System.out.println("\nPolicyholder's Last Name: " + getHolderLastName());
-      System.out.println("\nPolicyholder's Age: " + getHolderAge());
-      System.out.println("\nPolicyholder's Smoking Status (smoker/non-smoker): " + getSmokingStatus());
-      System.out.println("\nPolicyholder's Height: " + getHolderHeight() + " inches");
-      System.out.println("\nPolicyholder's Weight: " + getHolderWeight() + " pounds");
-      System.out.printf("\nPolicyholder's BMI: %.2f", getHolderBmi());                                                                     
-      System.out.printf("\n\nPolicy Price: $%,.2f", getPolicyPrice());
+      String str = "\nPolicy Number: " + getPolicyNumber() +
+                   "\nProvider Name: " + getProviderName() +
+                   "\n\nPolicy Price: $%,.2f" + getPolicyPrice();
+      
+      //return string
+      return str;
    }
    
-   //End display method
+   //End toString method
 }
